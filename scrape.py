f9e7ad96dc6f070fec63de8961a8ae774a3b4350
Brian Archibald <brian.archibald@imtapps.com>
2020-08-30 15:02:08 -0500

    Refactor: Added script for scraping

---
+++ scrape.py
@@ -0,0 +1,15 @@
+import os
+import selenium
+from selenium import webdriver
+import time
+from PIL import Image
+import io
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+search_url = "https://www.google.com/search?q={q}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568"
+driver.get(search_url.format(q='Car'))
